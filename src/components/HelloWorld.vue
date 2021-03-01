3ab2c670db134cbf230680c283b677854937f2a4
GBU_DanhNC <danh.nguyen@dsquare.com.vn>
2021-03-01 16:25:05 +0700

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+        <button type="button" @click="onSubmit()">Submit</button>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'HelloWorld',
+    data: function () {
+        return {
+            data: {}
+        }
+    },
+    created () {
+
+
+    },
+    methods: {
+        onSubmit() {
+
+            const options = {
+                method: 'GET',
+                url: 'http://api-with-cors.local/api/product',
+                params: {
+
+                },
+                headers: {
+                    'X-Accept': 'accept-header',
+                }
+            };
+
+            axios.request(options).then(function (response) {
+                console.log(response.data);
+            }).catch(function (error) {
+                console.error(error);
+            });
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
